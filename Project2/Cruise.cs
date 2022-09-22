774b29869b6020fbea8e3d0f7f9351bc6835b611
nenwam <nenwam333@gmail.com>
2022-09-21 17:32:57 -0700

    first commit

---
+++ Project2/Cruise.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Project2
+{
+    public delegate void priceCutEvent(Int32 pr);
+    internal class Cruise
+    {
+        static Random rng = new Random();
+        public static event priceCutEvent priceCut;
+        private static Int32 cruisePrice = 100;
+        public Int32 getPrice()
+        {
+            return cruisePrice;
+        }
+
+        public static void changePrice(Int32 price)
+        {
+            if (price < cruisePrice)
+            {
+                if(priceCut != null)
+                {
+                    priceCut(price); 
+                }
+            }
+            cruisePrice = price;
+        }
+
+        // This will have to be updated to take in different parameters to calculate the price.
+        public void priceModel()
+        {
+            // Implement the seasons by having an array of 4 seasons, then randomly picking which season out of that array
+            // Use same logic to implement the available tickets
+            for (Int32 i = 0; i < 20; i++)
+            {
+                Thread.Sleep(500);
+                Int32 p = rng.Next(40, 200);
+                Console.WriteLine("New price is: {0}", p);
+
+                Cruise.changePrice(p);
+            }
+        }
+    }
+}
---
+++ Project2/TicketAgent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Project2
+{
+    internal class TicketAgent
+    {
+        public void ticketAgentThread()
+        {
+            Cruise cruise = new Cruise();
+            for (Int32 i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Int32 p = cruise.getPrice();
+                Console.WriteLine("TicketAgent {0} has an everyday low ticket price of: {1} each", Thread.CurrentThread.Name, p);
+            }
+        }
+
+        public void ticketOnSale(Int32 p)
+        {
+            Console.WriteLine("Thread {0}: tickets are on sale for: {1} each", Thread.CurrentThread.Name, p);
+        }
+    }
+}
